7fcba1ca93645b82e1a20b62000dc253b7774d51
Mao Sasakura <huuchitaro@gmail.com>
2020-01-06 16:25:17 +0900

    fix 'serial' name conflict

--- serial_arduino.py
+++ serial_arduino.py
@@ -4,11 +4,11 @@ import time
 #シリアル通信(PC⇔Arduino)
 ser = serial.Serial()
 ser.port = "/dev/tty.usbmodem14111"     #デバイスマネージャでArduinoのポート確認
-ser.baudrate = 115200 #Arduinoと合わせる
+ser.baudrate = 9600 #Arduinoと合わせる
 ser.setDTR(False)     #DTRを常にLOWにしReset阻止
 ser.open()            #COMポートを開く
 for i in range(10):
-  ser.write(b'1')       #送りたい内容をバイト列で送信
+  ser.write(b'l')       #送りたい内容をバイト列で送信
   time.sleep(1)
   ser.write(b'0')
   time.sleep(1)
